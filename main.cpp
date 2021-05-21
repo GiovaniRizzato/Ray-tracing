1fc286bf0a11922d3f6d52ba12d2c103e724ad06
Giovani Rizzato <giovanirizzato@gmail.com>
2021-05-21 13:49:06 -0300

    Final capitulo 8 (implementação de materiais difusos)

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
         return color(0,0,0);
 
     if (world.hit(r, 0.001, infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        point3 target = rec.p + random_in_hemisphere(rec.normal);
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
@@ -50,10 +50,10 @@ int main() {
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 200;
+    const int image_width = 300;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 40;
-     const int max_depth = 50;
+    const int samples_per_pixel = 30;
+    const int max_depth = 40;
 
     // Camera
     camera cam;
--- vec3.h
+++ vec3.h
@@ -126,4 +126,16 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
+vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
+}
+
 #endif
